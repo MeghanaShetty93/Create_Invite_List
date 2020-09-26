df2b9fe8df14f1a407c6ef25cce43710992b98ab
MeghanaShetty93 <68819282+MeghanaShetty93@users.noreply.github.com>
2020-09-26 18:26:09 +0100

    Update upload_customer_details.py

--- upload_customer_details.py
+++ upload_customer_details.py
@@ -13,13 +13,16 @@ def get_details():
     custList = []
     
     #Upload text file
-    currentDirectory = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(currentDirectory, 'customers.txt')) as input:
+    try:
+        currentDirectory = os.path.dirname(os.path.abspath(__file__))
+        with open(os.path.join(currentDirectory, 'customers.txt')) as input:
    # with open('C:/Users/Dell/Anaconda3/FinalTest/customers.txt') as input :
-        for line in input:
-            custDict = json.loads(line)
-            custList.append(custDict)
-    return custList
+            for line in input:
+                custDict = json.loads(line)
+                custList.append(custDict)
+        return custList
+    except FileNotFoundError:
+        return 'Something went wrong, check if your file exists'
 
 
 # In[ ]:
